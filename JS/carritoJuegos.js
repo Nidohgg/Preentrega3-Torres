586e94a15e24c682e0ff37ed130da0ccd0aee302
Emanuel Torres <emauelema562@gmail.com>
2023-12-02 13:53:17 -0300

    Version 12

---
+++ JS/carritoJuegos.js
@@ -0,0 +1,34 @@
+console.log("Conectado");
+
+//array donde se guardaran los productos seleccionados
+let carrito = [];
+
+//operador 
+if(carrito.length >= 1){
+    alert("Ir a pagar");
+}else{
+    console.log("Usuario prefiere seguir viendo productos.");
+}
+
+//array de productos 
+const videojuegosArticulos = [
+
+    {nombre: "Kirby - Nintendo Switch", precio: 20000, descripcion: "insertar descripcion"},
+    {nombre: "Star Wars JEDI Fallen the Fallen Order - PS5", precio: 28000, descripcion: "insertar descripcion"},
+    {nombre: "Dead and Cells - Nintendo Switch", precio: 21000, descripcion: "insertar descripcion"},
+    {nombre: "Uncharted 4 - PS4", precio: 19000, descripcion: "insertar descripcion"},
+    {nombre: "Uncharted: A lost legacy - PS4", precio: 20000, descripcion: "insertar descripcion"},
+    {nombre: "Far Cry 6 - XBOX ", precio: 23000, descripcion: "insertar descripcion"},
+    {nombre: "CoD Modern Warfare III - PS4", precio: 20000, descripcion: "insertar descripcion"},
+    {nombre: "Dragon Ball Sagas - PC", precio: 3000, descripcion: "insertar descripcion"},
+    {nombre: "Sifu PS5", precio: 26000, descripcion: "insertar descripcion"},
+    {nombre: "Bloodborne PS4", precio: 15000, descripcion: "insertar descripcion"},
+    {nombre: "Resident Evil 2 - PS4", precio: 25000, descripcion: "insertar descripcion"},
+    {nombre: "Resident Evil Village PS5", precio: 26000, descripcion: "insertar descripcion"},
+
+]
+
+
+
+
+
